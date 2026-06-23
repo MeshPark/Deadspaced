26c8f129c4cf26b4bee45721d8f8b44f19bf5e37
MeshPark <irekkon@proton.me>
2026-06-23 12:56:15 +0100

    new git command

---
+++ Deadspaced/Commander/def.sh
@@ -0,0 +1,17 @@
+# Intialize a new repository
+git init
+
+# Adding files
+git add . # To add all files
+
+# Commiting
+git commit -m "slave to his own conviction"
+
+# Setting the branch
+git branch -M main
+
+# Setting the remote reposistory
+git remote add origin https://github.com/Meshp/Deadspaced.git
+
+# Pushing to the remote repository
+git push -u origin main
